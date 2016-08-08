f767140b819cbcf7480766480c4337b83782af20
Pedro Amador <pedroamador.rodriguez@gmail.com>
2016-08-08 14:32:10 +0200

    First functional without readme

--- checkfiledate.sh
+++ checkfiledate.sh
@@ -30,25 +30,55 @@ else
             itemtime[$items]=$(echo ${fileline} | awk '{print $2}')
             itempresence[$items]=$(echo ${fileline} | awk '{print $3}')
             itemrecursive[$items]=$(echo ${fileline} | awk '{print $4}')
-            echo -e "\tDirectory:${itemdirectory[$items]}"
-            echo -e "\tTime:${itemtime[$items]}"
-            echo -e "\tPresence flag:${itempresence[$items]}"
-            echo -e "\tRecursive flag:${itemrecursive[$items]}"
+            echo -e "\tDirectory......: ${itemdirectory[$items]}"
+            echo -e "\tTime...........: ${itemtime[$items]}"
+            echo -e "\tPresence flag..: ${itempresence[$items]}"
+            echo -e "\tRecursive flag.: ${itemrecursive[$items]}"
         fi
     done < filelist.txt
 fi
-echo -e  "Config reading done... make the checks\n"
+echo -e  "Config reading done... do the checks\n"
 
 for item in $(seq 1 $items)
 do
-    echo "- Check config $item"
+    find_command="find ${itemdirectory[$item]} ${maxdepth} -mtime ${itemtime[$item]} | egrep '.*' &> /dev/null"
+    echo "- Check config $item:"
+    echo -e -n "\tCheck type..: "
     if [ ${itemrecursive[$item]} -eq 0 ]
     then
+        echo -n "RECURSIVE / "
         maxdepth="-maxdepth 1"
     else
+        echo -n "NON RECURSIVE / "
         maxdepth=""
     fi
+    if [ ${itempresence[$item]} -eq 0 ]
+    then
+        echo -n "ABSENCE OF FILES / "
+    else
+        echo -n "PRESENCE OF FILES / "
+    fi
+    echo "TIME ${itemtime[$item]}" 
+    echo -e "\tCommand.....: ${find_command}"
+    echo -e -n "\tResult......: "
     # In this find: 0 means there is files, 1 means there is no files
-    find ${itemdirectory[$item]} ${maxdepth} -mtime ${itemtime[$item]} | egrep '.*' &> /dev/null
-    echo $?
+    eval "$find_command"
+    result=$?
+    if [ $result -eq 0 ]
+    then
+        if [ ${itempresence[$item]} -ne 0 ]
+        then
+            echo -n "OK!"
+        else
+            echo -n "error! Send mail"
+        fi
+    else
+        if [ ${itempresence[$item]} -eq 0 ]
+        then
+            echo -n "OK!"
+        else
+            echo -n " error! Send mail"
+        fi
+    fi
+    echo " (exit code ${result}) "
 done
