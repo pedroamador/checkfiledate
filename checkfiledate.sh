189a63c80e2673021aef786276b2a7fb9dbb7637
Pedro Amador <pedroamador.rodriguez@gmail.com>
2016-08-09 09:07:42 +0200

    Add exec mode notice

--- checkfiledate.sh
+++ checkfiledate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Check Local Environment
+echo "Checkfiledate (c) Pedro Amador 2016"
+echo
 
-# Commands Definition
-ECHO="echo -e"
+# Check Local Environment
 
 # Variables Definition
 COMMAND_LIST="mail find egrep"
@@ -16,9 +16,34 @@ do
 done
 if [[ $EXIT_CODE -eq 1 ]]
 then
-   $ECHO "\ncheckfiledate requires '${COMMAND}' command but is not installed"
-   $ECHO "\nExecution aborted\n"
+   echo -e "\ncheckfiledate requires '${COMMAND}' command but is not installed"
+   echo -e "\nExecution aborted\n"
    exit 1
+fi
+
+#
+# Function for print the script execution mode
+#
+print_exec_mode ()
+{
+   echo -e "\n\tEXECUTION MODE =>\t$0 [mailtest]\n"
+}
+
+# Check for optional "mailtest" parameter
+
+if [ $# -ne 0 ]
+then
+    if [ $# -ne 1 ]
+    then
+        print_exec_mode
+        exit 1
+    else
+        if [ "$1" != "mailtest" ]
+        then
+            print_exec_mode
+            exit 1
+        fi
+    fi
 fi
 
 cd $(dirname $0)
@@ -28,6 +53,7 @@ maillist=""
 if [ ! -f maillist.txt ]
 then
     echo "ERROR: There is not maillist.txt"
+    exit 1
 else
     while IFS='' read -r mailline || [[ -n "$mailline" ]]; do
         if [ "${mailline:0:1}" != "#" ] && [ "${mailline}" != "" ]
@@ -42,6 +68,7 @@ items=0
 if [ ! -f filelist.txt ]
 then
     echo "ERROR: There is not filelist.txt"
+    exit 1
 else
     while IFS='' read -r fileline || [[ -n "$fileline" ]]; do
         if [ "${fileline:0:1}" != "#" ] && [ "${fileline}" != "" ]
@@ -82,19 +109,20 @@ do
     echo -e -n "\tCheck type..: "
     if [ ${itemrecursive[$item]} -eq 0 ]
     then
-        echo -n "RECURSIVE / "
+        checktype="RECURSIVE"
         maxdepth="-maxdepth 1"
     else
-        echo -n "NON RECURSIVE / "
+        checktype="NON RECURSIVE"
         maxdepth=""
     fi
     if [ ${itempresence[$item]} -eq 0 ]
     then
-        echo -n "ABSENCE OF FILES / "
+        checktype="${checktype} / ABSENCE OF FILES"
     else
-        echo -n "PRESENCE OF FILES / "
+        checktype="${checktype} / PRESENCE OF FILES"
     fi
-    echo "TIME ${itemtime[$item]}" 
+    checktype="${checktype} / TIME ${itemtime[$item]}" 
+    echo ${checktype}
     echo -e "\tCommand.....: ${find_command}"
     echo -e -n "\tResult......: "
     # In this find: 0 means there is files, 1 means there is no files
@@ -104,17 +132,17 @@ do
     then
         if [ ${itempresence[$item]} -ne 0 ]
         then
-            echo -n "OK!"
+            echo "[OK]"
         else
-            echo -n "error!"
+            echo -n "[ERROR]"
             sendemail=1
         fi
     else
         if [ ${itempresence[$item]} -eq 0 ]
         then
-            echo -n "OK!"
+            echo "[OK]"
         else
-            echo -n " error!"
+            echo -n "[ERROR]"
             sendemail=1
         fi
     fi
@@ -122,15 +150,12 @@ do
     if [ $sendemail -eq 1 ]
     then
         returnvalue=1
-        echo -n "...Sending email..."
         for targetmail in ${maillist}
         do
-            echo -e -n "Sending mail to ${targetmail}..."
-            echo -e "Hostname: $(hostname -f)\nDate: $(date)\nCheck #${item}: ${itemcheck[$item]}\nCommand: ${find_command}\nResult:\n================ CUT HERE ================\n${result_console}\n================ CUT HERE ================" | mail ${targetmail} -s "[$(hostname)] CHECKFILEDATE ERROR"
+            echo -e -n "\t\tSending mail to ${targetmail}..."
+            echo -e "Hostname: $(hostname -f)\nDate: $(date)\nCheck #${item}\n- Config line: ${itemcheck[$item]}\n- Check type: ${checktype}\nCommand: ${find_command}\nResult:\n================ CUT HERE ================\n${result_console}\n================ CUT HERE ================" | mail ${targetmail} -s "[$(hostname)] CHECKFILEDATE ERROR"
             echo -e " [ok]"
         done
-
     fi
-    echo " (exit code ${result}) "
 done
 exit ${returnvalue}
